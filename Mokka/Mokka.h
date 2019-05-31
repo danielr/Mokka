f6c8576de37c683eda1eed93f1dc333655dc27f3
Daniel Rinser <git@danielrinser.de>
2019-05-31 16:00:11 +0200

    Add empty framework project

---
+++ Mokka/Mokka.h
@@ -0,0 +1,19 @@
+//
+//  Mokka.h
+//  Mokka
+//
+//  Created by Daniel Rinser on 31.05.2019.
+//  Copyright © 2019 Daniel Rinser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Mokka.
+FOUNDATION_EXPORT double MokkaVersionNumber;
+
+//! Project version string for Mokka.
+FOUNDATION_EXPORT const unsigned char MokkaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Mokka/PublicHeader.h>
+
+
---
+++ MokkaTests/MokkaTests.swift
@@ -0,0 +1,34 @@
+//
+//  MokkaTests.swift
+//  MokkaTests
+//
+//  Created by Daniel Rinser on 31.05.2019.
+//  Copyright © 2019 Daniel Rinser. All rights reserved.
+//
+
+import XCTest
+@testable import Mokka
+
+class MokkaTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
