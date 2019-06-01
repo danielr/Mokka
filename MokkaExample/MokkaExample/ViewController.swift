3299551d829265148fa27ec1bb2ba79c395b66bc
Daniel Rinser <git@danielrinser.de>
2019-06-01 23:07:38 +0200

    Add empty example project

---
+++ MokkaExample/MokkaExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MokkaExample
+//
+//  Created by Daniel Rinser on 01.06.2019.
+//  Copyright © 2019 Daniel Rinser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
